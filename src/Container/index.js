bcce50d00c77cec9f44d987c41336247f73d82bf
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-01 13:39:42 +0100

    added container component

---
+++ src/Container/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cards from '../Cards';
+
+function Container() {
+    return (
+        <div className='flex items-center justify-center min-h-screen bg-slate-800'>
+            <Cards />
+        </div>
+    )
+}
+
+export default Container;
